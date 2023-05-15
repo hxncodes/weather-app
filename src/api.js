4a9eeb7581bbeb0dc94b2a93abfe492145a203db
Hasnat Rana <hxncodes@gmail.com>
2023-05-15 06:45:59 +0500

    GeoDB Cities integrated

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiUrl = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities";
+
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "d563118cc7msh6c19431bb68a490p1082ebjsna06ca1f811ea",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,10 +1,22 @@
 import { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
+import { geoApiUrl, geoApiOptions } from "../../api";
 
 const Search = ({ onSearchChange }) => {
   const [search, setSearch] = useState(null);
 
-  const loadOptions = () => {};
+  const loadOptions = async (inputValue) => {
+    try {
+      const response = await fetch(
+        `${geoApiUrl}?minPopulation=100000&namePrefix=${inputValue}`,
+        geoApiOptions
+      );
+      const result = await response.json();
+      return console.log(result);
+    } catch (err) {
+      return console.log(err);
+    }
+  };
 
   const handleOnChange = (searchData) => {
     setSearch(searchData);
